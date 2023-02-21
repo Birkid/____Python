1ec137f5d94a42458743a6ba51c49afb2e558319
Dode <birkid@protonmai.com>
2023-02-22 01:14:55 +0800

    Python基础

--- 07）____字符串切片.py
+++ 07）____字符串切片.py
@@ -4,7 +4,7 @@
 name = 'china'
 name1 = name[:3]        #截止到第三位
 name2 = name[:-1]       #截止到倒数第一位
-name3 = name[1:4:2]     #从1开始到4结束，间隔2个步数
+name3 = name[1:4:2]     #从1开始到4结束，间隔2个步长
 print(name3)
 
 # slcie 切片
@@ -14,5 +14,9 @@ web2 = 'http://bilibili.com'
 #方法 1
 print(web1[slice(7,-4)])
 #方法2
-bilibili = slice(7,-4)      #   相对方法 1 多创建了一个变量
-print(web2[bilibili])
+bi = slice(7,-4)      #   相对方法 1 多创建了一个变量
+print(web2[bi])
+#方法3 给切片赋值
+web3 = list('baidu')
+web3[2:] = list('google')
+print(web3)
